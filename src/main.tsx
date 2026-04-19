6fad15fcba7cae7424f106c49a296aa910f5d86b
Mangalam <mangalamchaubey122@gmail.com>
2026-04-19 23:05:36 +0530

    Add analytics for react

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { Analytics } from "@vercel/analytics/next";
+import { Analytics } from "@vercel/analytics/react";
 import App from './App';
 import './index.css';
 import { useEngineStore, engineRuntime } from './store/useEngineStore';
